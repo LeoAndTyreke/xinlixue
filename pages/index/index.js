e76567cec9f6bd3a9d475f8922f7e4596c036e40
Tyreke <windybamb@163.com>
2018-09-19 18:33:42 +0800

    细节

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,71 +1,105 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var categories = [
+	{
+		img: 'http://www.familyktv.com/images/ys.png',
+		name: '月嫂'
+	},
+	{
+		img: 'http://www.familyktv.com/images/zntj.png',
+		name: '胎教师'
+	},
+	{
+		img: 'http://www.familyktv.com/images/yjs.png',
+		name: '幼教师'
+	},
+	{
+		img: 'http://www.familyktv.com/images/yycd.png',
+		name: '营养师'
+	},
+	{
+		img: 'http://www.familyktv.com/images/xlcp.png',
+		name: '心理师'
+	},
+	{
+		img: 'http://www.familyktv.com/images/xlcp.png',
+		name: '育婴师'
+	},
+	{
+		img: 'http://www.familyktv.com/images/xlcp.png',
+		name: '母婴护理'
+	},
+	{
+		img: 'http://www.familyktv.com/images/xlcp.png',
+		name: '产后护理'
+	}
+]
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    focusBool: false,
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onReady: function() {
-    this.lessonList = this.selectComponent('#lessonList');
-    this.teacherList = this.selectComponent('#teacherList');
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  inputFocus: function() {
-    console.log('onfocus')
-    this.setData({
-      focusBool: true
-    })
-  },
-  inputBlur: function() {
-    console.log('onblur')
-    this.setData({
-      focusBool: false
-    })
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+	data: {
+		categories,
+		userInfo: {},
+		focusBool: false,
+		hasUserInfo: false,
+		canIUse: wx.canIUse('button.open-type.getUserInfo')
+	},
+	//事件处理函数
+	bindViewTap: function () {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	onReady: function () {
+		this.lessonList = this.selectComponent('#lessonList');
+		this.teacherList = this.selectComponent('#teacherList');
+	},
+	onLoad: function () {
+		if (app.globalData.userInfo) {
+			this.setData({
+				userInfo: app.globalData.userInfo,
+				hasUserInfo: true
+			})
+		} else if (this.data.canIUse) {
+			// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+			// 所以此处加入 callback 以防止这种情况
+			app.userInfoReadyCallback = res => {
+				this.setData({
+					userInfo: res.userInfo,
+					hasUserInfo: true
+				})
+			}
+		} else {
+			// 在没有 open-type=getUserInfo 版本的兼容处理
+			wx.getUserInfo({
+				success: res => {
+					app.globalData.userInfo = res.userInfo
+					this.setData({
+						userInfo: res.userInfo,
+						hasUserInfo: true
+					})
+				}
+			})
+		}
+	},
+	inputFocus: function () {
+		console.log('onfocus')
+		this.setData({
+			focusBool: true
+		})
+	},
+	inputBlur: function () {
+		console.log('onblur')
+		this.setData({
+			focusBool: false
+		})
+	},
+	getUserInfo: function (e) {
+		console.log(e)
+		app.globalData.userInfo = e.detail.userInfo
+		this.setData({
+			userInfo: e.detail.userInfo,
+			hasUserInfo: true
+		})
+	}
 })
