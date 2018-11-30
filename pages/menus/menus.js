9d647937152952405fdc4a4b4d55b08d6884be37
leolee18 <wwwliyuxi@163.com>
2018-11-30 20:19:05 +0800

    自动滚动和项目数问题

--- pages/menus/menus.js
+++ pages/menus/menus.js
@@ -1,5 +1,6 @@
 let mLogin = require('../../utils/mLogin.js');
 let mMenD = require('../../utils/menusData.js');
+let mPh = require('../../utils/iPhone.js');
 Page({
   data: {
     mObj:{},
@@ -20,13 +21,18 @@ Page({
     let that = this;
     let mChid = options.id;
     let mcId = options.cid;
+    let mScTop = 0;
+    let mScTopPx = 0;
     mLogin.getUserInfo(function (mToken) {
       mMenD.getMenData(mToken, mChid, function (data) {
         let mCwLis = data.cwList;
         that.setData({ mObj: data.cc });
         mCwLis.forEach(function (val, ind) {
+          mScTop ++;
           val.children.forEach(function (cval, cind) {
+            mScTop++;
             if (mcId == cval.uid){
+              mScTopPx = mScTop * 98 * mPh.getWH();
               cval.vbool = true;
             }else{
               cval.vbool = false;
@@ -34,8 +40,15 @@ Page({
           })
         })
         that.setData({ mList: mCwLis });
+        that.moveScrollTop(mScTopPx-100);
       })
     });
+  },
+  moveScrollTop:function(mTop){
+    if (mTop < 0) mTop=0;
+    setTimeout(function(){
+      wx.pageScrollTo({ scrollTop: mTop, duration: 500 });
+    },500)
   },
   onShow: function () {
 
